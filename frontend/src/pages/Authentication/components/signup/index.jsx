d1d0ca5d1eb218df1d27bb2ad09b4497bbba6d6c
AliAbdelAall <3laych@gmail.com>
2024-03-24 00:11:57 +0200

    added vlaidatePassword function in Signup component

--- frontend/src/pages/Authentication/components/signup/index.jsx
+++ frontend/src/pages/Authentication/components/signup/index.jsx
@@ -1,24 +1,38 @@
 import "../../../../styles/common/utilities.css"
 import "../../../../styles/common/colors.css"
 import "./style.css"
+import { useState, useEffect } from "react"
 
 
 const Signup = () => {
   const [incorrect, setIncorrect] = useState(false)
   const [error, setError] = useState("User Already Exists")
-  const [credentials, setCredentials] = useState({f_name: "", l_name, email: "",password: "", confPassword: ""})
+  const [credentials, setCredentials] = useState({fName: "", lName: "", email: "",password: "", confPassword: ""})
     
+  const validatePassword = (password, confPassword) => {
+    if (password.length < 6){
+      setIncorrect(true)
+      setError("Password is Too Short")
+      return true
+    }else{
+      if (password.length !== confPassword.length){
+        setIncorrect(true)
+        setError("Passwords Doesn't match")
+        return true
+      }
+    return false
+  }
 
   return(
     <div className="flex column center auth-warapper">
       <h2 className="col-primary">LinkedIn</h2>
-      <input type="text" placeholder="First Name" onChange={(e) => setCredentials({...credentials, f_name: e.target.value})}/>
-      <input type="text" placeholder="Last Name" onChange={(e) => setCredentials({...credentials, l_name: e.target.value})}/>
+      <input type="text" placeholder="First Name" onChange={(e) => setCredentials({...credentials, fName: e.target.value})}/>
+      <input type="text" placeholder="Last Name" onChange={(e) => setCredentials({...credentials, lName: e.target.value})}/>
       <input type="text" placeholder="Example@email.com" onChange={(e) => setCredentials({...credentials, email: e.target.value})}/>
       <input type="password" placeholder="Password" onChange={(e) => setCredentials({...credentials, password: e.target.value})}/>
-      <input type="password" placeholder="Confirm-Password" onChange={(e) => setCredentials({...credentials, conf_password: e.target.value})}/>
+      <input type="password" placeholder="Confirm-Password" onChange={(e) => setCredentials({...credentials, confPassword: e.target.value})}/>
       <p className={`incorrect red ${ incorrect ? "" : "invisible" }`}>{`${error}`}</p>
-      <button className="bg-primary bold">Signup</button>
+      <button className="bg-primary bold" onClick={validSignup}>Signup</button>
       <p>Already have an account? <span className="col-primary bold">Login</span></p>
     </div>
   )

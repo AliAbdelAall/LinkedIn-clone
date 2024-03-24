8ad6fa5357fc15d41a3cd7365b2f1f183fc1edb4
AliAbdelAall <3laych@gmail.com>
2024-03-24 22:24:22 +0200

    added savePost API

---
+++ backend/savePost.php
@@ -0,0 +1,19 @@
+<?php
+include("connection.php");
+
+$content = $_POST['content'];
+$image = $_POST['image'];
+$user_id = $_POST['user_id'];
+
+$query = $mysqli -> prepare('INSERT INTO posts (content, image, user_id) VALUES (?, ?, ?)');
+$query -> bind_param("ssi", $content, $image, $user_id);
+if($query -> execute()){
+  $response['status'] = "sucsess";
+  $response['message'] = "post saved successfully";;
+
+}else{
+  $response['status'] = "failed";
+  $response['message'] = "post not saved";
+}
+
+echo json_encode($response);

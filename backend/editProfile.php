14155a330a3bc6109009e88f6de914d764a11be5
AliAbdelAall <3laych@gmail.com>
2024-03-26 04:04:31 +0200

    added editProfile API

--- backend/editProfile.php
+++ backend/editProfile.php
@@ -1,3 +1,26 @@
+<?php
+include("connection.php");
 
+$user_id = $_POST['user_id'];
+$user_profile = $_POST['user_profile'];
+$experience = $_POST['experience'];
+$skills = $_POST['skills'];
+$bio = $_POST['bio'];
+
+$query = $mysqli->prepare('
+UPDATE users
+SET user_profile = ?, experience = ?, skills = ?, bio = ?
+WHERE user_id = ?
+');
+$query->bind_param("ssssi",$user_profile, $experience, $skills, $bio, $user_id);
+if($query->execute()){
+  $response['status'] = "success";
+  $response['message'] = "profile updated successfully";
+}else{
+  $response['status'] = "failed";
+  $response['message'] = "profile updated failed";
+}
+
+echo json_encode($response);
 
 

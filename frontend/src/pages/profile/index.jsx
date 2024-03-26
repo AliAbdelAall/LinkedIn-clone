44a06685da6b5a9fb4667a66e45a38bb4758d179
AliAbdelAall <3laych@gmail.com>
2024-03-26 02:58:35 +0200

    updated Profile component for Profile page

--- frontend/src/pages/profile/index.jsx
+++ frontend/src/pages/profile/index.jsx
@@ -4,13 +4,14 @@ import "./style.css"
 import "../../styles/common/utilities.css"
 
 const Profile = ({userId}) => {
-  const [isopen, setisopen] = useState(false)
+  const [image, setImage] = useState(null)
+  const [isOpen, setIsOpen] = useState(false)
+  const [experience, setExperience] = useState(false)
+  const [skills, setSkills] = useState(false)
+  const [bio, setBio] = useState(false)
   const [userInfo, setUserInfo] = useState({})
 
   const loadUserInfo = async () => {
-
-    
-    console.log(userId)
     try{
       const response =  await fetch(
         `http://127.0.0.1/LinkedIn-clone/backend/getUserInfo.php?user_id=${userId}`,{
@@ -20,6 +21,10 @@ const Profile = ({userId}) => {
       const user = await response.json()
       if(user.status === "success"){
         setUserInfo(user.user_info)
+        setExperience(user.user_info.experience)
+        setSkills(user.user_info.skills)
+        setBio(user.user_info.bio)
+        setImage((user.user_info.profile_image))
         console.log(user.user_info)
 
       }
@@ -27,6 +32,39 @@ const Profile = ({userId}) => {
       console.error(error)
     }
   }
+
+const editProfile = () => {
+  try{
+    const result = fetch
+  }catch(error){
+    console.error(error)
+  }
+}
+
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0]
+    console.log(file.type)
+    if(file){
+      if (file.type === "image/png" ||
+        file.type === "image/jpg" ||
+        file.type === "image/jpeg"){
+        setIncorrect(false)
+      
+        console.log(file)
+
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          const result = reader.result
+          setImage(result)
+          
+        };
+        reader.readAsDataURL(file);
+        console.log(image)
+      }
+    } 
+  }
+
 
   useEffect(()=>{
     loadUserInfo ()
@@ -46,36 +84,40 @@ const Profile = ({userId}) => {
         <p>{`Bio: ${userInfo.bio}`}</p>
       </div>
 
-      <button className='edit-btn white bg-primary bold' onClick={()=> setisopen(true)}>Edit</button>
-      <div className={`flex center pupup ${isopen ? "" : "hidden"}`}>
+      <button className='edit-btn white bg-primary bold' onClick={()=> setIsOpen(true)}>Edit</button>
+      <div className={`flex center pupup ${isOpen ? "" : "hidden"}`}>
         <div className='flex column edit-popup'>
           <input 
               className='profile-input' 
               type="file" 
               placeholder='Uplaod image' 
               id='profile-input'
+              onChange={handleImageChange}
               />
           
           <input 
             className='info-input' type="text" 
-            placeholder='Experience' 
-            // value ={userInfo.experience}
+            placeholder='Experience'
+            value={experience}
+            onChange={(e)=> setExperience(e.target.value)}
             /> 
 
           <input 
             className='info-input' type="text" 
             placeholder='Skills' 
-            // value ={userInfo.skills}
+            value={skills}
+            onChange={(e)=> setSkills(e.target.value)}
             /> 
               
           <input 
             className='info-input' type="text" 
-            placeholder='Bio' 
-            // value ={userInfo.bio} 
+            placeholder='Bio'
+            value={bio} 
+            onChange={(e)=> setBio(e.target.value)}
             />    
           <div className='flex space-between'>
-            <button className='edit-btn white bg-primary bold'>Confirm</button>
-            <button className='edit-btn white bg-primary bold' onClick={(e)=> setisopen(false)}>Cancel</button>
+            <button className='edit-btn white bg-primary bold' on>Confirm</button>
+            <button className='edit-btn white bg-primary bold' onClick={(e)=> setIsOpen(false)}>Cancel</button>
           </div>
         </div>
       </div>
